22d6779eef79d41e2c4be912dc0e072652e054c1
TrueRaisins <idanik.ishot@gmail.com>
2023-11-02 01:01:06 +0300

    initial commit

---
+++ src/main/java/Animal/main.java
@@ -0,0 +1,13 @@
+package Animal;
+
+public class main {
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        Fish fish = new Fish();
+        Duck duck = new Duck();
+
+        cat.move();
+        fish.move();
+        duck.move();
+    }
+}
